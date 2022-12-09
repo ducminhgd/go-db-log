36c8242c801aa68393f0336a2689fe98a61ce84c
Giã Dương Đức Minh <giaduongducminh@gmail.com>
2022-12-09 15:18:30 +0700

    Add create and count method

--- models.go
+++ models.go
@@ -1,19 +1,29 @@
 package models
 
-import "time"
+import (
+	"time"
 
-type Timestamp time.Time
+	"gorm.io/gorm"
+)
 
 type EventLog struct {
-	ID         string    `gorm:"primarykey,column:id"`
-	EventType  string    `gorm:"column:event_type"`
-	ObjectType string    `gorm:"column:object_type"`
-	ObjectID   string    `gorm:"column:object_id"`
-	ActorType  string    `gorm:"column:actor_type"`
-	ActorID    string    `gorm:"column:actor_id"`
-	Data       string    `gorm:"column:data"`
-	Result     int32     `gorm:"column:result"`
-	Timestamps Timestamp `gorm:"column:timestamps"`
+	ID         string    `gorm:"primarykey,column:id" json:"id"`
+	EventType  string    `gorm:"column:event_type" json:"eventType"`
+	ObjectType string    `gorm:"column:object_type" json:"objectType"`
+	ObjectID   string    `gorm:"column:object_id" json:"objectID"`
+	ActorType  string    `gorm:"column:actor_type" json:"actorType"`
+	ActorID    string    `gorm:"column:actor_id" json:"actorID"`
+	Data       string    `gorm:"column:data" json:"data"`
+	Result     int32     `gorm:"column:result" json:"result"`
+	Timestamp  time.Time `gorm:"column:timestamp" json:"timestamp"`
+}
+
+type QueryParams struct {
+	Limit  int64
+	Offset int64
+	Result int32
+	From   time.Time
+	To     time.Time
 }
 
 // TableName returns the name of event logging table
@@ -22,3 +32,16 @@ func (EventLog) TableName() string {
 }
 
 type EventLogs []*EventLog
+
+type Clause func(tx *gorm.DB)
+
+func Create(db *gorm.DB, el *EventLog) (*EventLog, error) {
+	err := db.Model(&EventLog{}).Create(el).Error
+	return el, err
+}
+
+func Count(db *gorm.DB, params QueryParams) (int64, error) {
+	var count int64
+	tx := db.Model(&EventLogs{}).Count(&count)
+	return count, tx.err
+}
